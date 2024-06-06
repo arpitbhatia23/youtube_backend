8defae69ac779031b9ee4788c23b0b358b27cd4b
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-06-06 19:30:37 +0530

    added

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constant.js";
 const connectDB=async()=>{
     try {
   const connectionInstance=   await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
- console.log(`\n MongoDB connected !! DB HODT :${connectionInstance.connection.host}`)
+ console.log(`\n MongoDB connected !! DB HOST at  :${connectionInstance.connection.host}`)
         
     } catch (error) {
         console.log("connection error",error)
