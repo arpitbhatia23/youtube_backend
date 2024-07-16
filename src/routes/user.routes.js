e17d7e12bc4d6569fd8f85d2cbae033bd1c43e72
Arpit <156578394+arpitbhatia23@users.noreply.github.com>
2024-07-16 09:59:29 +0530

    added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,6 +13,8 @@ import {
   import {upload} from '../middlewares/multer.middleware.js'
   import {verifyJwt} from '../middlewares/auth.middleware.js'
 const router =Router()
+
+
     router.route("/register").post(upload.fields([
         {name:"avatar",
         maxCount:1
